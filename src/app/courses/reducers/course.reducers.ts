52df5c6d91e75f4a8819b336b289c26c708392ce
Juan Pablo Ramírez <kandarv@gmail.com>
2022-04-03 18:13:07 +0000

    Using NgRx EntityFormat

--- src/app/courses/reducers/course.reducers.ts
+++ src/app/courses/reducers/course.reducers.ts
@@ -1,10 +1,8 @@
+import { EntityState } from "@ngrx/entity";
 import { Course } from "../model/course";
 
 
-// this is the entity format
-export interface CourseState {
-  // entity is a map whose keys are id's of the entities
-  entities: {[key: number]: Course},
-  // store the order of the entities
-  ids: number[]
+// using EntityState 
+export interface CourseState extends EntityState<Course> {
+
 }
