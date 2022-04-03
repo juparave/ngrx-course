2a3220b003f2d0af4ef87b2aa9afc6c762368156
Juan Pablo Ramírez <kandarv@gmail.com>
2022-04-03 18:43:10 +0000

    NgRx Entity Selectors, refactoring HomeComponent

--- src/app/courses/reducers/course.reducers.ts
+++ src/app/courses/reducers/course.reducers.ts
@@ -20,3 +20,5 @@ export const coursesReducer = createReducer(
   on(CourseActions.allCoursesLoaded,
     (state, action) => adapter.addAll(action.courses, state))
 )
+
+export const {selectAll} = adapter.getSelectors();
