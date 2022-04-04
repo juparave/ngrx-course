31595633347a97f02c179840f6ffe22136b1ac18
Juan Pablo Ramírez <kandarv@gmail.com>
2022-04-04 16:58:43 +0000

    Optimistically editing entity data

--- src/app/courses/course.actions.ts
+++ src/app/courses/course.actions.ts
@@ -1,4 +1,5 @@
 import { createAction, props } from "@ngrx/store";
+import { Update } from "@ngrx/entity";
 import { Course } from "./model/course";
 
 
@@ -10,3 +11,9 @@ export const allCoursesLoaded = createAction(
   "[Load Courses Effect] All Courses Loaded",
   props<{courses: Course[]}>()
 );
+
+export const courseUpdated = createAction(
+  "[Edit Course Dialog] Course Updated",
+  props<{update: Update<Course>}>()
+);
+
--- src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
+++ src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
@@ -1,9 +1,14 @@
-import {Component, Inject} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
-import {Course} from '../model/course';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {Observable} from 'rxjs';
-import {CoursesHttpService} from '../services/courses-http.service';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Course } from '../model/course';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { CoursesHttpService } from '../services/courses-http.service';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../reducers';
+import {Update} from '@ngrx/entity';
+import {courseUpdated} from '../course.actions';
+
 
 @Component({
   selector: 'course-dialog',
@@ -26,7 +31,7 @@ export class EditCourseDialogComponent {
     private fb: FormBuilder,
     private dialogRef: MatDialogRef<EditCourseDialogComponent>,
     @Inject(MAT_DIALOG_DATA) data,
-    private coursesService: CoursesHttpService) {
+    private store: Store<AppState>) {
 
     this.dialogTitle = data.dialogTitle;
     this.course = data.course;
@@ -63,11 +68,16 @@ export class EditCourseDialogComponent {
       ...this.form.value
     };
 
-    this.coursesService.saveCourse(course.id, course)
-      .subscribe(
-        () => this.dialogRef.close()
-      )
+    // build update object to pass it to the action courseUpdated
+    const update: Update<Course> = {
+      id: course.id,
+      changes: course
+    };
 
+    // dispatch the action with update object
+    this.store.dispatch(courseUpdated({update}));
+
+    this.dialogRef.close();
 
   }
 
